1587402933121348a87f8eb8801cd9f26db91c59
Rekutur <zirkonyalee31@gmail.com>
2025-05-04 13:24:38 +0200

    Belge Düzenlendi + Animasyonlar Eklendi

--- jamGame/Assets/Items/ItemDefinition.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item Definition")]
-public class ItemDefinition : ScriptableObject
-{
-    public string itemName;
-    public Sprite icon;
-}
